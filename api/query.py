a7d1b258dd1d3911a8d8df3f6b14412d09a6c8df
Elmer Rodriguez <elmer.rodriguezn@gmail.com>
2019-09-04 09:05:25 -0700

    fixed problem with statics files in  settings.py

--- api/query.py
+++ api/query.py
@@ -5,11 +5,11 @@ class Connection:
     models, db, uid, password = (models, db, uid, password)
 
     def execute(self, model, operation, query, fields=''):
-        return models.execute_kw(db, uid, password, model, operation, [query], fields)
+        return models.execute_kw(self.db, self.uid, self.password, model, operation, [query], fields)
 
 
 class Query(Connection):
-    def get(self, model, operation, query='', fields=''):
+    def get(self, model, operation, query, fields=''):
         return super().execute(model, operation, query, fields)
 
     def create(self, model, operation, query):
