d035f0ac90af83b07129fa501d9a42c3ece28a5d
Elmer Rodriguez <elmer.rodriguezn@gmail.com>
2019-10-08 09:36:07 -0700

    Fixed secret key recaptcha

--- phe_project/settings.py
+++ phe_project/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-GOOGLE_RECAPTCHA_SECRET_KEY = '6LfyirwUAAAAAMdO7_uTg4GCiNCnKkT9xyrCrJun'
+GOOGLE_RECAPTCHA_SECRET_KEY = '_SECRET_KEY_'
 
 try:
     from .local_settings import *
